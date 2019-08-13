fc645b0e0d71ca507ad73c9bfd1fd0d5ce26c608
RonaldRamoss <ronaldvalentimramos@gmail.com>
2019-08-13 13:09:08 +0900

    Project's Initial Commit

---
+++ www/components/leve.js
@@ -0,0 +1,35 @@
+
+$(document).on("click","#env",function(){
+var sexo = parseFloat($("#sexo").val());
+
+if(sexo == 1){
+
+var al = parseFloat($("#altura").val());
+var altura = al * 100;
+var kilos = parseFloat($("#kilos").val());
+var idade = $("#idade").val();
+var taxaAtividade = 1200;
+var primeiraConta = parseFloat(13.7) * kilos;
+var segundaConta = parseFloat(5) * altura;
+var terceiraConta = primeiraConta + segundaConta; 
+var quartaConta = parseFloat(6.8) * idade;
+var quintaConta = terceiraConta - quartaConta;
+var sextaConta = quintaConta + 66;
+$("#resultado").val(sextaConta);
+}
+else{
+  var al = parseFloat($("#altura").val());
+var altura = al * 100;
+var kilos = parseFloat($("#kilos").val());
+var idade = $("#idade").val();
+var taxaAtividade = 1200;
+var primeiraConta = parseFloat(9.6) * kilos;
+var segundaConta = parseFloat(1.8) * altura;
+var terceiraConta = primeiraConta + segundaConta; 
+var quartaConta = parseFloat(4.7) * idade;
+var quintaConta = terceiraConta - quartaConta;
+var sextaConta = quintaConta + 655;
+$("#resultado").val(sextaConta);
+}
+
+});
---
+++ www/components/mode.js
@@ -0,0 +1,37 @@
+
+$(document).on("click","#env",function(){
+var sexo = parseFloat($("#sexo").val());
+
+if(sexo == 1){
+
+var al = parseFloat($("#altura").val());
+var altura = al * 100;
+var kilos = parseFloat($("#kilos").val());
+var idade = $("#idade").val();
+var taxaAtividade = 1375;
+var primeiraConta = parseFloat(13.7) * kilos;
+var segundaConta = parseFloat(5) * altura;
+var terceiraConta = primeiraConta + segundaConta; 
+var quartaConta = parseFloat(6.8) * idade;
+var quintaConta = terceiraConta - quartaConta;
+var sextaConta = quintaConta + 66;
+var setimaConta = sextaConta * taxaAtividade;
+$("#resultado").val(setimaConta);
+}
+else{
+  var al = parseFloat($("#altura").val());
+var altura = al * 100;
+var kilos = parseFloat($("#kilos").val());
+var idade = $("#idade").val();
+var taxaAtividade = 1375;
+var primeiraConta = parseFloat(9.6) * kilos;
+var segundaConta = parseFloat(1.8) * altura;
+var terceiraConta = primeiraConta + segundaConta; 
+var quartaConta = parseFloat(4.7) * idade;
+var quintaConta = terceiraConta - quartaConta;
+var sextaConta = quintaConta + 655;
+var setimaConta = sextaConta * taxaAtividade;
+$("#resultado").val(setimaConta);
+}
+
+});
